36bfc4554ef586b78ab800478c6228c1468dbf8a
Romajonn <hmmnohmmno2@gmail.com>
2026-07-29 14:46:34 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 from telebot import types
 
 # Tokeningizni yozing
-API_TOKEN = '8721986149:AAHun68uIGe7Rm_3mi2q5sb9iQHmJVd40Kg'
+API_TOKEN = '8721986149:AAHun68uIGe7Rm_3mi2q5sb9iQHmJVd4OKg'
 
 # Kanal username'ini yozing
 CHANNEL_USERNAME = '@Namangan_toshkent3'
